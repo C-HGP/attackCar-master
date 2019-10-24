c301a12c9d313ad5d6712f4e0b1d021818ce04f3
RobDra <robin.drakenhufvud@gmail.com>
2019-10-24 11:58:05 +0200

    Updated

--- src/main.cpp
+++ src/main.cpp
@@ -24,13 +24,17 @@ int pin_left_forward = D10;
 int pin_left_backwards = D11;
 int pin_right_forward = D4;
 int pin_right_backwards = D6;
+int led_red = A5;
+int led_green = A4;
 bool go = true;
+bool turning = false;
 
 TaskHandle_t xHandle = NULL;
 
 //Declare functions
 void drive(void *pvParameters);
 void randomizer(void *pvParameters);
+void turn(void *pvParameters);
 
 void setup() {
   xTaskCreate(
@@ -45,6 +49,15 @@ void setup() {
   xTaskCreate(
       randomizer,
       (const portCHAR *)"Random", // Human readable name
+      (configSTACK_DEPTH_TYPE)64, // Stack size
+      (void *)NULL,               // A pvParameters to use by the task
+      (UBaseType_t)1,             // Priority, 3 (configMAX_PRIORITIES - 1) highest, 0 lowest
+      (TaskHandle_t *)NULL        // Task handle for external manipulation of task
+  );
+
+  xTaskCreate(
+      turn,
+      (const portCHAR *)"Turn", // Human readable name
       (configSTACK_DEPTH_TYPE)64, // Stack size
       (void *)NULL,               // A pvParameters to use by the task
       (UBaseType_t)1,             // Priority, 3 (configMAX_PRIORITIES - 1) highest, 0 lowest
@@ -83,17 +96,50 @@ void drive(void *pvParameters) {
     vTaskDelay(5);
   }
 }
+void turn(void *pvParameters) {
+  for (;;)
+  {
+    pinMode(pin_left_forward, OUTPUT);
+    pinMode(pin_left_backwards, OUTPUT);
+    pinMode(pin_right_forward, OUTPUT);
+    pinMode(pin_right_backwards, OUTPUT);
+    if(turning == true)
+    {
+      int duration = random(1000, 5000);
+      int rightOrLeft = random(1,2);
 
+      
+        digitalWrite(pin_right_forward, HIGH);
+        digitalWrite(pin_left_forward, LOW);
+        digitalWrite(pin_right_backwards, LOW);
+        digitalWrite(pin_left_backwards, LOW);
+      
+     /* else
+      {
+        digitalWrite(pin_right_forward, LOW);
+        digitalWrite(pin_left_forward, HIGH);
+        digitalWrite(pin_right_backwards, LOW);
+        digitalWrite(pin_left_backwards, LOW);
+      }*/
+    }
+    vTaskDelay(5);
+  }
+}
 //Function to randomize a stop for the robot
 void randomizer(void *pvParameters)
 {
   for (;;)
   {
+    pinMode(pin_left_forward, OUTPUT);
+    pinMode(pin_left_backwards, OUTPUT);
+    pinMode(pin_right_forward, OUTPUT);
+    pinMode(pin_right_backwards, OUTPUT);
     int randNr = random(1, 5);
     if (randNr == 3)
     {
       go = false;
+      turning = true;
     }
-    vTaskDelay(5000);
+    vTaskDelay(1000);
   }
 }
