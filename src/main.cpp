c8944e9d9e664acece150a2455f8310947dd7a11
RobDra <robin.drakenhufvud@gmail.com>
2019-10-24 11:11:15 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,6 @@ By Robin & CHP
 2019
 
 */ 
-
-#include <Arduino.h>
-#include <STM32FreeRTOS.h>
-
-int pin_left_forward = D10;
-int pin_left_backwards = D11;
-int pin_right_forward = D3;
-int pin_right_backwards = D5;
-
-TaskHandle_t xHandle = NULL;
-void drive(void *pvParameters);
-
-
 /**
  * #TODO
  * - ADD TASK FOR RANDOM GENERATION
@@ -28,35 +15,42 @@ void drive(void *pvParameters);
  * 
  * */
 
-enum Directions {
-  Forward,
-  Backwards,
-  Still,
-  Left,
-  Right,
-  Circle
-};
-int state = 2;
-int previous_btn, current_btn;
+#include <Arduino.h>
+#include <STM32FreeRTOS.h>
+
+int pin_left_forward = D10;
+int pin_left_backwards = D11;
+int pin_right_forward = D3;
+int pin_right_backwards = D5;
+bool go = false;
+
+TaskHandle_t xHandle = NULL;
+void drive(void *pvParameters);
+
+void randomizer(void *pvParameters);
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(pin_left_forward, OUTPUT); //left forward
-  pinMode(pin_left_backwards, OUTPUT); //left forward
-  pinMode(pin_right_forward, OUTPUT); //left forward
-  pinMode(pin_right_backwards, OUTPUT); //left forward
-  pinMode(USER_BTN, INPUT); //nucleos ugly pin >:(
-
+  
   xTaskCreate(
         drive,
         (const portCHAR *)"Go",   // Human readable name
         (configSTACK_DEPTH_TYPE)64,   // Stack size
-        (void *)1,                // A pvParameters to use by the taskk
+        (void *)NULL,                // A pvParameters to use by the task
         (UBaseType_t)1,               // Priority, 3 (configMAX_PRIORITIES - 1) highest, 0 lowest
         (TaskHandle_t *)NULL          // Task handle for external manipulation of task
     );
-  vTaskStartScheduler();
   
+  xTaskCreate(
+        randomizer,
+        (const portCHAR *)"Random",   // Human readable name
+        (configSTACK_DEPTH_TYPE)64,   // Stack size
+        (void *)NULL,                // A pvParameters to use by the task
+        (UBaseType_t)1,               // Priority, 3 (configMAX_PRIORITIES - 1) highest, 0 lowest
+        (TaskHandle_t *)NULL          // Task handle for external manipulation of task
+    );
+
+  vTaskStartScheduler();
 }
 void loop() {
   
@@ -64,13 +58,33 @@ void loop() {
 
 void drive(void *pvParameters)
 {
-  
-    for(;;) {
-    digitalWrite(pin_right_forward, LOW);
-    digitalWrite(pin_left_forward, LOW);
-    digitalWrite(pin_right_backwards, LOW);
-    digitalWrite(pin_left_backwards, LOW);  
-      vTaskDelay(100);
+  for(;;)
+  {
+    pinMode(pin_left_forward, OUTPUT); 
+    pinMode(pin_left_backwards, OUTPUT); 
+    pinMode(pin_right_forward, OUTPUT); 
+    pinMode(pin_right_backwards, OUTPUT);
+    if(go)
+    { 
+      digitalWrite(pin_right_forward, HIGH);
+      digitalWrite(pin_left_forward, HIGH);
+      digitalWrite(pin_right_backwards, LOW);
+      digitalWrite(pin_left_backwards, LOW);
     }
+    vTaskDelay(5);
+  }
+}
+
+void randomizer(void *pvParameters)
+{
+  for(;;)
+  {
+    int randNr = random(1,5);
+    if(randNr == 3)
+    {
+      go = false;
+    }
+    vTaskDelay(5000);
+ }
 }
 
