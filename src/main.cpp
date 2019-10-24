4ffe05b35b2cfc890e7e16719cceb3a03203ea3e
CHGP <exocorpllc@gmail.com>
2019-10-24 14:24:22 +0200

    Fucking STM shit fuck

--- src/main.cpp
+++ src/main.cpp
@@ -20,12 +20,18 @@ By Robin & CHP
 #include <STM32FreeRTOS.h>
 
 //Declare pins
-int pin_left_forward = D10;
-int pin_left_backwards = D11;
-int pin_right_forward = D4;
-int pin_right_backwards = D6;
+const int pin_left_forward = D10;
+const int pin_left_backwards = D11;
+const int pin_right_forward = D4;
+const int pin_right_backwards = D6;
+const int trigPin = D7;
+const int echoPin = D8;
+long duration, cm;
+
 int led_red = A5;
 int led_green = A4;
+
+//bullar
 bool go = true;
 bool turning = false;
 bool left = true;
@@ -34,13 +40,15 @@ TaskHandle_t xHandle = NULL;
 
 //Declare functions
 void drive(void *pvParameters);
-void randomizer(void *pvParameters);
-void turn(void *pvParameters);
+void sensor(void *pvParameters);
 
 void setup() {
+
+  Serial.begin (9600);
+
   xTaskCreate(
       drive,
-      (const portCHAR *)"Go",     // Human readable name
+      (const portCHAR *)"DriveF",     // Human readable name
       (configSTACK_DEPTH_TYPE)64, // Stack size
       (void *)NULL,               // A pvParameters to use by the task
       (UBaseType_t)1,             // Priority, 3 (configMAX_PRIORITIES - 1) highest, 0 lowest
@@ -48,23 +56,13 @@ void setup() {
   );
 
   xTaskCreate(
-      randomizer,
-      (const portCHAR *)"Random", // Human readable name
+      sensor,
+      (const portCHAR *)"Sensor",     // Human readable name
       (configSTACK_DEPTH_TYPE)64, // Stack size
       (void *)NULL,               // A pvParameters to use by the task
       (UBaseType_t)1,             // Priority, 3 (configMAX_PRIORITIES - 1) highest, 0 lowest
       (TaskHandle_t *)NULL        // Task handle for external manipulation of task
   );
-
-  xTaskCreate(
-      turn,
-      (const portCHAR *)"Turn", // Human readable name
-      (configSTACK_DEPTH_TYPE)64, // Stack size
-      (void *)NULL,               // A pvParameters to use by the task
-      (UBaseType_t)1,             // Priority, 3 (configMAX_PRIORITIES - 1) highest, 0 lowest
-      (TaskHandle_t *)NULL        // Task handle for external manipulation of task
-  );
-
   vTaskStartScheduler();
 }
 void loop() {
@@ -79,70 +77,30 @@ void drive(void *pvParameters) {
     pinMode(pin_left_backwards, OUTPUT);
     pinMode(pin_right_forward, OUTPUT);
     pinMode(pin_right_backwards, OUTPUT);
-
-    if (go)
-    {
-      digitalWrite(pin_right_forward, HIGH);
-      digitalWrite(pin_left_forward, HIGH);
-      digitalWrite(pin_right_backwards, LOW);
-      digitalWrite(pin_left_backwards, LOW);
-    }
-    else
-    {
-      digitalWrite(pin_right_forward, LOW);
-      digitalWrite(pin_left_forward, LOW);
-      digitalWrite(pin_right_backwards, LOW);
-      digitalWrite(pin_left_backwards, LOW);
-    }
-    vTaskDelay(5);
   }
 }
-void turn(void *pvParameters) {
-  for (;;)
-  {
-    pinMode(pin_left_forward, OUTPUT);
-    pinMode(pin_left_backwards, OUTPUT);
-    pinMode(pin_right_forward, OUTPUT);
-    pinMode(pin_right_backwards, OUTPUT);
-    if(turning == true)
-    {
-      
-      if(left == true)
-      {
-        digitalWrite(pin_right_forward, HIGH);
-        digitalWrite(pin_left_forward, LOW);
-        digitalWrite(pin_right_backwards, LOW);
-        digitalWrite(pin_left_backwards, LOW);
 
-        left = false;
-      }
-      if(left == false)
-      {
-        digitalWrite(pin_right_forward, LOW);
-        digitalWrite(pin_left_forward, HIGH);
-        digitalWrite(pin_right_backwards, LOW);
-        digitalWrite(pin_left_backwards, LOW);
-        left = true;
-      }
-    }
-    vTaskDelay(5);
-  }
-}
-//Function to randomize a stop for the robot
-void randomizer(void *pvParameters)
-{
+
+void sensor(void *pvParameters) {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
   for (;;)
   {
-    pinMode(pin_left_forward, OUTPUT);
-    pinMode(pin_left_backwards, OUTPUT);
-    pinMode(pin_right_forward, OUTPUT);
-    pinMode(pin_right_backwards, OUTPUT);
-    int randNr = random(1, 5);
-    if (randNr == 3)
-    {
-      go = false;
-      turning = true;
-    }
-    vTaskDelay(1000);
+    digitalWrite(trigPin, LOW);
+    vTaskDelay(50);
+    digitalWrite(trigPin, HIGH);
+    vTaskDelay(70);
+    digitalWrite(trigPin, LOW);
+
+
+    pinMode(echoPin, INPUT);
+    duration = pulseIn(echoPin, HIGH);
+    cm = (duration/2) / 29.1;
+  
+    Serial.print(cm);
+    Serial.print("cm");
+    Serial.println();
+
+    vTaskDelay(250);
   }
 }
