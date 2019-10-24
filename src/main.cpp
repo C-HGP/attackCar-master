7e5767a4d0f31871843375ae7cd5d0f26078bb94
CHGP <exocorpllc@gmail.com>
2019-10-24 11:26:50 +0200

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ The objective is to attack harmless walls at incredible speed.
 By Robin & CHP
 2019
 
-*/ 
+*/
 /**
  * #TODO
  * - ADD TASK FOR RANDOM GENERATION
@@ -15,9 +15,11 @@ By Robin & CHP
  * 
  * */
 
+//Libaries
 #include <Arduino.h>
 #include <STM32FreeRTOS.h>
 
+//Declare pins
 int pin_left_forward = D10;
 int pin_left_backwards = D11;
 int pin_right_forward = D4;
@@ -25,54 +27,54 @@ int pin_right_backwards = D6;
 bool go = true;
 
 TaskHandle_t xHandle = NULL;
-void drive(void *pvParameters);
 
+//Declare functions
+void drive(void *pvParameters);
 void randomizer(void *pvParameters);
 
 void setup() {
-  // put your setup code here, to run once:
-  
   xTaskCreate(
-        drive,
-        (const portCHAR *)"Go",   // Human readable name
-        (configSTACK_DEPTH_TYPE)64,   // Stack size
-        (void *)NULL,                // A pvParameters to use by the task
-        (UBaseType_t)1,               // Priority, 3 (configMAX_PRIORITIES - 1) highest, 0 lowest
-        (TaskHandle_t *)NULL          // Task handle for external manipulation of task
-    );
-  
+      drive,
+      (const portCHAR *)"Go",     // Human readable name
+      (configSTACK_DEPTH_TYPE)64, // Stack size
+      (void *)NULL,               // A pvParameters to use by the task
+      (UBaseType_t)1,             // Priority, 3 (configMAX_PRIORITIES - 1) highest, 0 lowest
+      (TaskHandle_t *)NULL        // Task handle for external manipulation of task
+  );
+
   xTaskCreate(
-        randomizer,
-        (const portCHAR *)"Random",   // Human readable name
-        (configSTACK_DEPTH_TYPE)64,   // Stack size
-        (void *)NULL,                // A pvParameters to use by the task
-        (UBaseType_t)1,               // Priority, 3 (configMAX_PRIORITIES - 1) highest, 0 lowest
-        (TaskHandle_t *)NULL          // Task handle for external manipulation of task
-    );
+      randomizer,
+      (const portCHAR *)"Random", // Human readable name
+      (configSTACK_DEPTH_TYPE)64, // Stack size
+      (void *)NULL,               // A pvParameters to use by the task
+      (UBaseType_t)1,             // Priority, 3 (configMAX_PRIORITIES - 1) highest, 0 lowest
+      (TaskHandle_t *)NULL        // Task handle for external manipulation of task
+  );
 
   vTaskStartScheduler();
 }
 void loop() {
-  
+  // FreeRTOS based, does not use loop
 }
 
-void drive(void *pvParameters)
-{
-  for(;;)
+// Function for casually driving
+void drive(void *pvParameters) {
+  for (;;)
   {
-    pinMode(pin_left_forward, OUTPUT); 
-    pinMode(pin_left_backwards, OUTPUT); 
-    pinMode(pin_right_forward, OUTPUT); 
+    pinMode(pin_left_forward, OUTPUT);
+    pinMode(pin_left_backwards, OUTPUT);
+    pinMode(pin_right_forward, OUTPUT);
     pinMode(pin_right_backwards, OUTPUT);
-    
-    if(go)
-    { 
+
+    if (go)
+    {
       digitalWrite(pin_right_forward, HIGH);
       digitalWrite(pin_left_forward, HIGH);
       digitalWrite(pin_right_backwards, LOW);
       digitalWrite(pin_left_backwards, LOW);
     }
-    else{
+    else
+    {
       digitalWrite(pin_right_forward, LOW);
       digitalWrite(pin_left_forward, LOW);
       digitalWrite(pin_right_backwards, LOW);
@@ -82,16 +84,16 @@ void drive(void *pvParameters)
   }
 }
 
+//Function to randomize a stop for the robot
 void randomizer(void *pvParameters)
 {
-  for(;;)
+  for (;;)
   {
-    int randNr = random(1,5);
-    if(randNr == 3)
+    int randNr = random(1, 5);
+    if (randNr == 3)
     {
       go = false;
     }
     vTaskDelay(5000);
- }
+  }
 }
-
